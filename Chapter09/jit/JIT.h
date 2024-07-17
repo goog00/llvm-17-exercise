19f56dc097ec5edfc30004d4c45194c03f79bf8d
code_st <1035802659@qq.com>
2024-07-17 13:46:13 +0800

    编写 jit

---
+++ Chapter09/jit/JIT.h
@@ -0,0 +1,29 @@
+#ifndef JIT_H
+#define JIT_H
+
+#include "llvm/Analysis/AliasAnalysis.h"
+#include "llvm/ExecutionEngine/JITSymbol.h"
+#include "llvm/ExecutionEngine/Orc/CompileUtils.h"
+#include "llvm/ExecutionEngine/Orc/Core.h"
+#include "llvm/ExecutionEngine/Orc/ExecutionUtils.h"
+#include "llvm/ExecutionEngine/Orc/IRCompileLayer.h"
+#include "llvm/ExecutionEngine/Orc/IRTransformLayer.h"
+#include "llvm/ExecutionEngine/Orc/JITTargetMachineBuilder.h"
+#include "llvm/ExecutionEngine/Orc/Mangling.h"
+#include "llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h"
+#include "llvm/ExecutionEngine/Orc/ExecutorProcessControl.h"
+#include "llvm/ExecutionEngine/SectionMemoryManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Support/Error.h"
+
+
+class JIT {
+
+
+private:
+    std::unqiue_ptr<llvm::ExecutorProcessControl> EPC;
+        
+}
+
+
+#endif
